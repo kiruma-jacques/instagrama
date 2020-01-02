1f0be9ac476c99592b40ce2ae63d39a8146dcf70
jakesIII <jayruma@yahoo.com>
2020-01-02 13:10:23 +0300

    configure the instaApp and uploadcare extention

--- instaProject/settings.py
+++ instaProject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instaApp',
+    'pyuploadcare.dj',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
