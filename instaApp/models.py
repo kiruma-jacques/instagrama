927133c429b70eb0d0ba1ec4dc1ffc68174a13fb
jakesIII <jayruma@yahoo.com>
2020-01-02 13:27:45 +0300

    create two models; image and profile

--- instaApp/models.py
+++ instaApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length=30)
+    avatar = ImageField(manual_crop='')
+    bio = models.CharField(max_length=200)
+
+class Image(models.Model):
+    image = ImageField()
+    iname = models.CharField(max_length=30)
+    icaption = models.TextField(blank=True)
+    iprofile = models.ForeignKey(Profile)
+    
