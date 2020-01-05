8945e72dfa527356e566d087d5f616ebf5a1ad6a
jakesIII <jayruma@yahoo.com>
2020-01-05 20:32:19 +0300

    install django registration and bootstrap

--- instaProject/urls.py
+++ instaProject/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('instaApp.urls'))
+    url(r'', include('instaApp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
