dc61cfd649ee7a3d05c65ff7c476a0efb0e160d9
jakesIII <jayruma@yahoo.com>
2020-01-05 22:01:31 +0300

    add modal feature however not responsive

--- instaApp/views.py
+++ instaApp/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from .forms import UploadForm
 from .models import Image
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
-    posts=Image.objects.all()
+    posts=Image.objects.all()[::-1]
     if request.method == "POST":
-        upload_form = UploadForm(request.POST)
+        upload_form = UploadForm(request.POST, request.FILES)
         if upload_form.is_valid():
             upload = upload_form.save(commit=False)
             upload.save()
-            HttpResponseRedirect('homePage')
+            HttpResponseRedirect(reverse('homePage'))
     else:
         upload_form = UploadForm()
     return render (request, 'index.html',{"upload_form":upload_form, "posts":posts} )
--- instaProject/settings.py
+++ instaProject/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'instaApp.apps.InstaappConfig',
     'pyuploadcare.dj',
-    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
