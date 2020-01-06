57da780ed042bd39231b30abd401a6b1d64e8102
jakesIII <jayruma@yahoo.com>
2020-01-06 13:11:36 +0300

    add django user to models and resolved modal and dropdown bugs

--- instaApp/models.py
+++ instaApp/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
-    username = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     avatar = ImageField(manual_crop='')
     bio = models.CharField(max_length=200)
 
 class Image(models.Model):
     image = ImageField()
-    iname = models.CharField(max_length=30)
-    icaption = models.TextField(blank=True)
-    # iprofile = models.ForeignKey(Profile)
-    ilikes = models.IntegerField(default=0)
+    name = models.CharField(max_length=30)
+    caption = models.TextField(blank=True)
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField(default=0)
 
 class Comments(models.Model):
     comment = models.TextField(blank=True)
