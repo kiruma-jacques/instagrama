2cdd000a67455f9211ba904dfeb01f72c1f2bf85
jakesIII <jayruma@yahoo.com>
2020-01-08 17:30:41 +0300

    deployment

--- instaProject/settings.py
+++ instaProject/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # email configurations
 EMAIL_USE_TLS=config('EMAIL_USE_TLS')
