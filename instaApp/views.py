3a1f3812d1e461ee7f2a9a77216a0bd9bf672694
jakesIII <jayruma@yahoo.com>
2020-01-08 07:54:28 +0300

    display user avatar and update user profile

--- instaApp/views.py
+++ instaApp/views.py
@@ -3,6 +3,7 @@ from .forms import UploadForm,ProfileUpdateForm
 from .models import Image, Profile, Comments
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 ##################from .email import send_welcome_email
 
 # Create your views here.
@@ -27,21 +28,22 @@ def index(request,**kwargs):
     return render (request, 'index.html',locals())
 
 def profile(request):
-    current_profile=Profile.objects.exclude(id=request.user.id)
-    users_posts=Image.objects.filter(user=request.user)[::-1]
+    current_user = request.user
+    current_profile = Profile.objects.get(user=request.user)
+    users_posts = Image.objects.filter(user=request.user)[::-1]
+    update_form = ProfileUpdateForm(request.POST, instance=request.user)
     if request.method == "POST":
-        profile_update=ProfileUpdateForm(request.POST, request.FILES)
-        # import pdb; pdb.set_trace()
-        if profile_update.is_valid():
-            updated = profile_update.save(commit=False)
-            updated.user = request.user
-            updated.save()
+        if update_form.is_valid():
+            update = update_form.save(commit=False)
+            update.user = current_user
+            update.save
             return HttpResponseRedirect(request.path_info)
     else:
-        profile_update=ProfileUpdateForm()
+        update_form = ProfileUpdateForm()
     context={
-        'users_posts':users_posts,
-        'update_form':profile_update,
-        'user':current_profile,
+        'user':current_user,
+        'profile':current_profile,
+        'posts':users_posts,
+        'form':update_form,
     }
-    return render(request, 'profile.html', context)
+    return render(request, 'profile.html', locals())
