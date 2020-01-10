d24068095c659d7a7742fe43e2cf807a8e8cb255
jakesIII <jayruma@yahoo.com>
2020-01-10 09:50:17 +0300

    run 3 tests on Profile case

--- instaApp/models.py
+++ instaApp/models.py
@@ -4,14 +4,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    avatar = ImageField(manual_crop='')
+    avatar = ImageField(manual_crop='', null=True)
     bio = models.CharField(max_length=200)
 
     def __str__(self):
         return self.user.username
 
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile=cls.objects.all()
+
+    @classmethod
+    def single_profile(cls, id):
+        single_profile = cls.objects.filter(id=id)
+        return single_profile
+
 class Image(models.Model):
-    image = ImageField()
+    image = ImageField(null=True)
     name = models.CharField(max_length=30)
     caption = models.TextField(blank=True)
     likes = models.IntegerField(default=0)
@@ -20,6 +32,18 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def get_all_images(cls):
+        all_images=cls.objects.all()
+        return all_images
+
+    @classmethod
+    def get_single_img(cls, id):
+        one_img=cls.objects.filter(id=id)
 
 class Comments(models.Model):
     comment = models.TextField(blank=True)
--- instaApp/tests.py
+++ instaApp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Profile, Image
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.testuser=User(username='Cole')
+
+        self.testProfile=Profile(user=self.testuser, avatar='', bio='WWWiii')
+        self.testProfile.save_profile()
+
+    def test_instance_profile(self):
+        self.assertTrue(isinstance(self.testProfile, Profile))
+
+    def test_save_profile(self):
+        self.testuseth=User(username='21xsavage')
+        testprof=Profile(user=self.testuseth, avatar='', bio='WWWiii')
+        testprof.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(Profile)>0)
+
+    def test_single_profile(self, id):
+        i_profile = Profile.single_profile(id=id)
+        self.assertTrue(i_profile is not None)
+        
